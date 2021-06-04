fb94a778e9fe11e8378e09f840f42c2b4ffcdeb1
junyoshimura57 <jun.yoshimura57@gmail.com>
2021-06-04 22:31:15 +0900

    :wrench: generatorsの設定変更
    generatorsコマンド時の自動生成を防ぎたいファイル設定を記載

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ module ComfortableTrip
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # generatorsコマンド時の自動生成を防ぎたいファイルを以下に記載
+    config.generators do |g|
+      # css,jsのファイルは作成せず
+      g.assets false
+      # コントローラー作成時にconfig/routes.rbが書き換えられないようにする。
+      g.skip_routes true
+      # テストファイルは作成せず
+      g.test_framework false
+      # ヘルパーファイルも作成せず
+      g.helper false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
