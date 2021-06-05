20f917e7158c0b9348a94f813177ba9333c5279f
junyoshimura57 <jun.yoshimura57@gmail.com>
2021-06-05 11:24:39 +0900

    :wrench: タイムゾーンの変更
    タイムゾーンをJSTの設定に変更

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,11 @@ module ComfortableTrip
       g.helper false
     end
 
+    # TimeWithZoneオブジェクトにする際にTokyoのオブジェクトにする。
+    config.time_zone = 'Tokyo'
+    # DBに保存する時間もJSTにする。
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
