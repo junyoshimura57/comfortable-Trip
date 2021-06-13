b430c347c48312aa6c9de51476867b7d0b211348
junyoshimura57 <jun.yoshimura57@gmail.com>
2021-06-13 14:15:27 +0900

    :construction: userテーブルadminにデフォルト値を追加
    userテーブルadminにデフォルト値を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_043823) do
+ActiveRecord::Schema.define(version: 2021_06_13_051124) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2021_06_13_043823) do
     t.integer "living_prefecture", null: false
     t.float "min_favorite_high_temperature", null: false
     t.float "favorite_precipitation", null: false
-    t.boolean "admin", null: false
+    t.boolean "admin", default: false, null: false
     t.float "max_favorite_high_temperature", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
