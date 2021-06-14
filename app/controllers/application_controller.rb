cc05a9a2c965ea21e15a67801a97624e9a697b74
junyoshimura57 <jun.yoshimura57@gmail.com>
2021-06-14 22:52:08 +0900

    :recycle: rubocop -aを実行
    rubocop -aにてコード修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :email, :username, :password, :password_confirmation, :living_prefecture,  :min_favorite_high_temperature, :max_favorite_high_temperature, :favorite_precipitation, :admin]
+    added_attrs = %i[email username password password_confirmation living_prefecture min_favorite_high_temperature max_favorite_high_temperature favorite_precipitation admin]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
